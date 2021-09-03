0233a18b1fce6dd8a0dea4ac25724e5284a80ada
Kibugu <james.kibugu@gmail.com>
2021-09-03 13:17:31 +0300

    H

--- djangoresume1/settings.py
+++ djangoresume1/settings.py
@@ -1,9 +1,9 @@
+from pathlib import Path
+
 import os
 import django_heroku
 from decouple import config
 import dj_database_url
-
-from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -114,11 +114,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATICFILES_DIRS = [
-os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
+
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
